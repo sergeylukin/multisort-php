55a8fea69ca25fc5744f8e8a2ca2cdeda6329501
Sergey Lukin <contact@sergeylukin.com>
2014-07-21 12:31:39 +0000

    Adjust error message occured when PHPUnit runs before autloading is set up

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
-  die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+  die('You must set up the project autoloading, run the following commands:'.PHP_EOL.
     'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-    'php composer.phar install'.PHP_EOL);
+    'php composer.phar dumpautoload -o'.PHP_EOL);
 }
 
 $loader->add('Multisort\Test', __DIR__);
